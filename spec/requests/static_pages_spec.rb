c689ab3d181cfe625a982790db01e5958c5e0866
Peak Xu <pxu@carbonite.com>
2012-06-13 06:48:21 -0400

    Refactoring tests and adding headers and footers

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,17 +4,14 @@ describe "Static pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
-    before(:each) do
-      visit '/static_pages/home'
-    end
+    before { visit root_path }
 
     it "should have the h1 'Sample App'" do
-      page.should have_selector('h1', :text => 'Sample App')
+      page.should have_selector('h1', text: 'Sample App')
     end
 
     it "should have the base title" do
-      page.should have_selector('title',
-        :text => base_title)
+      page.should have_selector('title', text: full_title(''))
     end
 
     it "should not have a custom title" do
@@ -23,47 +20,57 @@ describe "Static pages" do
   end
  
   describe "Help page" do
-    before(:each) do
-      visit '/static_pages/help'
-    end
+    before { visit help_path }
 
     it "should have the h1 'Help'" do
-      page.should have_selector('h1', :text => 'Help')
+      page.should have_selector('h1', text: 'Help')
     end
 
     it "should have the right title" do
       page.should have_selector('title',
-        :text => "#{base_title} | Help")
+        text: full_title('Help'))
     end
   end
 
   describe "About page" do 
-    before(:each) do
-      visit '/static_pages/about'
-    end
+    before { visit about_path }
 
     it "should have the h1 'About Us'" do
-      page.should have_selector('h1', :text => 'About Us')
+      page.should have_selector('h1', text: 'About Us')
     end
 
     it "should have the right title" do
       page.should have_selector('title',
-        :text => "#{base_title} | About Us")
+        text: full_title('About Us'))
     end
   end
 
   describe "Contact page" do 
-    before(:each) do
-      visit '/static_pages/contact'
-    end
+    before { visit contact_path }
 
     it "should have the h1 'About Us'" do
-      page.should have_selector('h1', :text => 'Contact')
+      page.should have_selector('h1', text: 'Contact')
     end
 
     it "should have the right title" do
       page.should have_selector('title',
-        :text => "#{base_title} | Contact")
+        text: full_title('Contact'))
     end
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    page.should have_selector 'title', text: full_title('')
+    # click_link "Sign up now!"
+    # page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'h1', text: 'Sample App'
   end
 end
