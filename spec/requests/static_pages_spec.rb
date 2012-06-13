c0234c146e8890d785a339dc03d0e0ae2b2cdc30
Peak Xu <pxu@carbonite.com>
2012-06-13 07:56:55 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,61 +1,36 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home page" do
     before { visit root_path }
 
-    it "should have the h1 'Sample App'" do
-      page.should have_selector('h1', text: 'Sample App')
-    end
+    it { should have_selector('h1', text: 'Sample App') }
 
-    it "should have the base title" do
-      page.should have_selector('title', text: full_title(''))
-    end
-
-    it "should not have a custom title" do
-      page.should_not have have_selector(" | Home")
-    end
+    it { page.should have_selector('title', text: full_title('')) }
+    it { page.should_not have have_selector(" | Home") }
   end
  
   describe "Help page" do
     before { visit help_path }
 
-    it "should have the h1 'Help'" do
-      page.should have_selector('h1', text: 'Help')
-    end
-
-    it "should have the right title" do
-      page.should have_selector('title',
-        text: full_title('Help'))
-    end
+    it { page.should have_selector('h1', text: 'Help') }
+    it { page.should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do 
     before { visit about_path }
 
-    it "should have the h1 'About Us'" do
-      page.should have_selector('h1', text: 'About Us')
-    end
-
-    it "should have the right title" do
-      page.should have_selector('title',
-        text: full_title('About Us'))
-    end
+    it { page.should have_selector('h1', text: 'About Us') }
+    it { page.should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact page" do 
     before { visit contact_path }
 
-    it "should have the h1 'About Us'" do
-      page.should have_selector('h1', text: 'Contact')
-    end
-
-    it "should have the right title" do
-      page.should have_selector('title',
-        text: full_title('Contact'))
-    end
+    it { page.should have_selector('h1', text: 'Contact') }
+    it { page.should have_selector('title', text: full_title('Contact')) }
   end
 
   it "should have the right links on the layout" do
@@ -68,8 +43,8 @@ describe "Static pages" do
     page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
     page.should have_selector 'title', text: full_title('')
-    # click_link "Sign up now!"
-    # page.should have_selector 'title', text: full_title('Sign up')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
     click_link "sample app"
     page.should have_selector 'h1', text: 'Sample App'
   end
