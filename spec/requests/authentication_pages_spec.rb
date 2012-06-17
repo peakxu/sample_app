9235262a85daa53ae0ddae84a88cbeddbbb4778d
Peak Xu <pxu@carbonite.com>
2012-06-17 16:09:59 -0400

    Decoupling some tests from their implementation

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,8 +7,7 @@ describe "AuthenticationPages" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_selector('h1',     text: 'Sign in') }
-    it { should have_selector('title',  text: full_title('Sign in')) }
+    it { should be_on_sign_in_page }
   end
 
   describe "signin" do
@@ -19,7 +18,7 @@ describe "AuthenticationPages" do
     describe "with invalid information" do
       before { click_button signin }
 
-      it { should have_selector('title',  text: full_title('Sign in')) }
+      it { should be_on_sign_in_page }
       it { should have_error_message }
 
       describe "after visiting another page" do
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -5,3 +5,10 @@ RSpec::Matchers.define :have_error_message do |message|
     page.should have_selector('div.alert.alert-error', text: 'Invalid')
   end
 end
+
+RSpec::Matchers.define :be_on_sign_in_page do |message|
+  match do |page|
+    page.should have_selector('title',  text: full_title('Sign in'))
+    page.should have_selector('h1',     text: 'Sign in')
+  end
+end
